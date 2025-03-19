46e71932265ed6d91c767d1ae1979f9596f61885
ChristopherEdlly <Christopher.edlly@souunit.com.br>
2025-03-19 14:14:37 -0300

    alteração do cors

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    // Configura o CORS para permitir o frontend
     app.enableCors({
-        origin: 'http://localhost:5173', // Permite a origem do frontend (ajuste conforme necessário)
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
-        allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
+        origin: [
+            "https://tcc-despesas-front-end.vercel.app",
+            "http://localhost:5173",  // mantendo para desenvolvimento local
+            "http://localhost:3000"   // mantendo para desenvolvimento local
+        ],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Authorization',
+        credentials: true
     });
 
-    await app.listen(3000);
+    // Usar porta dinâmica para o Render
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Aplicação rodando na porta ${port}`);
 }
 
 bootstrap();
